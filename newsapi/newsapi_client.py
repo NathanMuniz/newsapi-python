8d7ddfdf207bac902efc710d25e7a9ddfcb1bfd4
NathanMuniz <nathan.jogos987@gmail.com>
2022-07-08 23:38:17 -0300

    fiz alugnas coisa imporatanes do client

--- newsapi/newsapi_client.py
+++ newsapi/newsapi_client.py
@@ -1,3 +1,4 @@
+from pytz import country_timezones
 import requests
 from newsapi import const
 from newsapi.newsapi_auth import NewsApiAuth 
@@ -42,7 +43,7 @@ class NewsApiClient(object):
 
 
     def get_top_headlines( # noqa: C901
-    self, q=None, qintile=None, sources=None, langauge="en", country=None, category=None, page_size=None, page=None
+    self, q=None, qintitle=None, sources=None, langauge="en", country=None, category=None, page_size=None, page=None
 
     ):
 
@@ -54,9 +55,43 @@ class NewsApiClient(object):
         the earliest date published first.
 
         :param q: Keywords or a phase to search for in the article title and body. 
-    
-        
+        :type q: str or None
+
+        :param qintitle: Keywords to a phrase to search for in the article title and body.
+        :type sintle: str
+
+        :param sources: A comma-separates string of indentifier for the news sources or blog you 
+        wnat headline from.
+        :param:
+
+        :param language: The 2-letter ISO-639-1 code fo the languge you wuant ot get headlines for.
+        :type languge: str or None
+
+        :param country: The 2-letter ISO 3166-1 code fo the contry you qnat ot get headlines for.
+        :type country: str or None
+
+        :param category: The category you want to get headlines for.
+        :type category: str or None
+
+        :param_size: Use this to page throught the results fi the total resutla found is greater thean
+        the page size.
+        :type page_size: int or None
+
+        :param page: The number of results to return per page (request).
+            20 is the default, 100 is the maximum.
+        :type page: int or None
+
+        return: JSON resposne as nasted Python dictionary.
+        :rtype: dict
+        :raises NewsAPIException: If the ``"status"`` value of the resposne is ``"error"`` rather than ``"ok"``. 
+
+
         """
+
+
+        # Cria objeto Paylod, que será usado para tranferir as coisas importantes da API.
+
+        payload = {} 
 
         # Verifica se a variável q foi passada
             # Se foi -> irá irá verificar se o q é uma string válida, usando a validação do newspia
@@ -71,4 +106,86 @@ class NewsApiClient(object):
                 raise TypeError("Keyword/phrase q param shoud be of type str")
             
 
-        #
+        # Keyword/Phrase in Title. Mesma coisa do q.
+        if qintitle is not None:
+            if is_valid_string(qintitle):
+                payload["qintitle"] = qintitle
+            else:
+                raise TypeError("Keyword/phrase qintitle param sould be of type str")
+        
+
+        # Sources -> mesma coisa do q.
+        if sources is not None:
+            payload["sources"] = sources 
+        else: 
+            raise TypeError("srouces param shoud be type of str")
+
+        # Lenguagem -> mesma coisa do q.
+        if langauge is not None:
+            if is_valid_string(sources):
+                payload["sources"] = sources 
+            else: 
+                raise TypeError("srouces param soud be of type str")
+        
+        # Controy , mesma coisa porém ele verifica que o contry passado está na lista de cost de
+        # contries
+        if country is not None:
+            if is_valid_string(country):
+                if country in const.countries:
+                    payload["country"] = country
+                else:
+                    raise ValueError("invalid country")
+            else: 
+                raise TypeError("country param shoud be of type str")
+        
+        #Category - mesma coisa porém verifica se a categoria está nas constantes
+        if category is not None:
+            if is_valid_string(category):
+                if category in const.categories:
+                    payload["category"] - category
+                else:
+                    raise ValueError("invalid category")
+            else:
+                raise TypeError("category param shoud be of type str")
+
+
+
+
+        # Page size, verifica se o tipo é int. Verficair se ele está entre o e 100. Tem um else 
+        # para cada if
+        if page_size is not None:
+            if type(page_size) == int:
+                if 0 <= page_size <= 100:
+                    payload["pageSize"] = page_size 
+                else:
+                    raise ValueError("page_size param should b an int between 1 and 100")
+            else:
+                raise ValueError("page_size param shoud be an int")
+        
+        # Page - mesma coisa do page_size, porém tem que ser apenas mairo que 0
+        if page is not None:
+            if type(page) == int:
+                if page > 0:
+                    payload["page"] = page 
+                else:
+                    raise ValueError("Page param shoud be an int greater than 0")
+            else:
+                raise TypeError("page param shoud be an int")
+        
+        # Send Request - usa o request_method para fazer um get. Ele irá passar os seguinte parametos
+        # url: Pagara da nossa cont, url de headline
+        # auth: que será nosso auth
+        # timeout: será 30
+        # params: será payload
+        # Ele joga toda essa requisição get, dentro da variável r
+        r = self.request_method.get(const.TOP_HEADLINES_URL, auth=self.auth, timeout=30, params=payload)
+
+        # Check Status of Request  - Verifica se nossa requisão tive um códido ruim, ou sejá
+        # usando o requests.codes.ok, como comparação
+        # Se naõ teve um retorno ok, então ele irá lançar um object NewAPIExeption, e passara um 
+        # json, da nossa requisição
+        if r.status_code != requests.codes.ok:
+            raise NewsAPIException(r.json())
+        
+        # então ele retorna o json da nossa requisição
+        return r.json()
