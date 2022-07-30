60e920406388326f58b4b2e95c13a9efd1cdb77b
NathanMuniz <natangabriel.palmeiras@gmail.com>
2022-07-30 16:27:08 -0300

    Finished the utils, and finished the client (for now...)

--- newsapi/newsapi_client.py
+++ newsapi/newsapi_client.py
@@ -1,1 +1,76 @@
 
+from newsapi_auth import NewsApiAuth
+from utils import is_valid_string, stringify_date_param 
+
+import requests
+
+import const
+from newsapi_exception import NewsApiExeception
+
+
+
+class NewsApiClient(object):
+
+    def __init__(self, api_key, session=None):
+        self.auth = NewsApiAuth(api_key=api_key)
+        if session is not None: 
+            self.conxtext_request = session
+        else:
+            self.conxtext_request = requests
+
+    def get_top_headline(self, q=None, qintitle=None, source=None, language=None, country=None):
+        
+        payload = {}
+
+        if q is not None:
+            if is_valid_string(q):
+                payload['q'] = q
+            else:
+                TypeError('q must be a valid string')
+
+        if qintitle is not None:
+            if is_valid_string(qintitle):
+                payload['qintitle'] = q
+            else:
+                TypeError('qintitle must be a valid string')
+
+        
+        if source is not None and ((country is not None) or language is not None):
+            raise ValueError("The source cannot be passed mixed with coutry or language")
+
+        if source is not None:
+            if is_valid_string(source):
+                payload['source'] = source
+            else:
+                TypeError('q must be a valid string')
+        
+        if language is not None:
+            if is_valid_string(language):
+                if language in const.languages:
+                    payload['language'] = language
+                else:
+                    ValueError("Language must to be in the sigla format")
+            else:
+                TypeError("Lanauge must to be a valid string")
+
+        if country is not None:
+            if is_valid_string(country):
+                if country in const.countries:
+                    payload['country'] = country
+                else:
+                    ValueError("Country must to be in the sigla format")
+            else:
+                TypeError("Coutry must to be a valid string")
+        
+        r = self.conxtext_request.get(const.TOP_HEADLINES_URL, self.auth, timeout=30, params=payload)
+
+        if r.request_code != requests.request_code_ok:
+            raise NewsApiExeception(r.json())
+
+        return r.json()
+
+
+
+
+
+
--- newsapi/utils.py
+++ newsapi/utils.py
@@ -4,38 +4,73 @@ import datetime
 import re
 import sys
 
-# Definimos um nome para os modulos, quando importar esse módulo inteiro
-# Esse nome é stringfy_date_param
-
-__all__ = ("stringfy_date_param",)
+__all__ = ("stringify_date_param",)
 
 
-#  [ valid_string ]
-# Iremo criar duas const, PY2 e PY3, que pegam a versão do python
-# ele usa o sys
-# Irá criar uma função de is_valid_string, porém irá
-# primeiro verificar se a a versão, e para cada versão irá ter uma
-# valid string diferent. 
-    # Se a for versão 3
-        # a função valid string irá apenas verificar se a variávels
-        # passada é um instancia de str
-    # Se for a versão 2 
-        # irá verificar se a variável passada é basestring
-    # se não fo nenhum desse
-        # irá lançar erro, que o python não suporta esse tipo.
-        # Esse erro é um System erro 
+# Date in ISO-8601 format
+DATE_RE = re.compile(r"^\d{4}-\d{2}-\d{2}$")
+DATE_LEN = len("YYYY-MM-DD")
+DATE_FMT = "%Y-%m-%d"
 
-# [ validate_dateime_str ]
-# recebe parâmtro uma datastr
-# Verfifica se a 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# #
+# Datetime in ISO-8601 format
+DATETIME_RE = re.compile(r"^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$")
+DATETIME_LEN = len("YYYY-MM-DDTHH:MM:SS")
+DATETIME_FMT = "%Y-%m-%dT%H:%M:%S"
+
+
+def stringify_date_param(dt):
+    if is_valid_string(dt):
+        if len(dt) == DATE_LEN:
+            validate_date_str(dt)
+        elif len(dt) == DATETIME_LEN:
+            validate_datetime_str(dt)
+        else:
+            raise ValueError("Date input should be in format of either YYYY-MM-DD or YYYY-MM-DDTHH:MM:SS")
+        return dt
+    # Careful: datetime.datetime is subclass of datetime.date!
+    elif isinstance(dt, datetime.datetime):
+        # TODO: time zone
+        return dt.strftime(DATETIME_FMT)
+    elif isinstance(dt, datetime.date):
+        return dt.strftime(DATE_FMT)
+    elif is_valid_num(dt):
+        return datetime.datetime.utcfromtimestamp(dt).strftime(DATETIME_FMT)
+    else:
+        raise TypeError("Date input must be one of: str, date, datetime, float, int, or None")
+
+
+def validate_date_str(datestr):
+    if not DATE_RE.match(datestr):
+        raise ValueError("Date input should be in format of YYYY-MM-DD")
+
+
+def validate_datetime_str(datetimestr):
+    if not DATETIME_RE.match(datetimestr):
+        raise ValueError("Datetime input should be in format of YYYY-MM-DDTHH:MM:SS")
+
+
+PY2 = sys.version_info[0] == 2
+PY3 = sys.version_info[0] == 3
+
+if PY3:
+
+    def is_valid_string(var):
+        return isinstance(var, str)
+
+    def is_valid_num(var):
+        return isinstance(var, (int, float))
+
+
+elif PY2:
+
+    def is_valid_string(var):
+        return isinstance(var, basestring)  # noqa
+
+    def is_valid_num(var):
+        return isinstance(var, (int, float, long))  # noqa
+
+
+else:
+
+    def is_valid_string(var):
+        raise SystemError("unsupported version of python detected (supported versions: 2, 3)")
