57f02814372b4a3b71201bc23d82a9b4c626899b
NathanMuniz <natangabriel.palmeiras@gmail.com>
2022-07-30 16:31:11 -0300

    add some more modification

--- newsapi/newsapi_client.py
+++ newsapi/newsapi_client.py
@@ -1,71 +1,159 @@
-
-from newsapi_auth import NewsApiAuth
-from utils import is_valid_string, stringify_date_param 
+from __future__ import unicode_literals
 
 import requests
 
-import const
-from newsapi_exception import NewsApiExeception
-
+from newsapi import const
+from newsapi.newsapi_auth import NewsApiAuth
+from newsapi.newsapi_exception import NewsAPIException
+from newsapi.utils import is_valid_string, stringify_date_param
 
 
 class NewsApiClient(object):
+    """The core client object used to fetch data from News API endpoints.
+    :param api_key: Your API key, a length-32 UUID string provided for your News API account.
+        You must `register <https://newsapi.org/register>`_ for a News API key.
+    :type api_key: str
+    :param session: An optional :class:`requests.Session` instance from which to execute requests.
+        **Note**: If you provide a ``session`` instance, :class:`NewsApiClient` will *not* close the session
+        for you.  Remember to call ``session.close()``, or use the session as a context manager, to close
+        the socket and free up resources.
+    :type session: `requests.Session <https://2.python-requests.org/en/master/user/advanced/#session-objects>`_ or None
+    """
 
     def __init__(self, api_key, session=None):
         self.auth = NewsApiAuth(api_key=api_key)
-        if session is not None: 
-            self.conxtext_request = session
+        if session is None:
+            self.request_method = requests
         else:
-            self.conxtext_request = requests
+            self.request_method = session
 
-    def get_top_headline(self, q=None, qintitle=None, source=None, language=None, country=None):
-        
+    def get_top_headlines(  # noqa: C901
+        self, q=None, qintitle=None, sources=None, language="en", country=None, category=None, page_size=None, page=None
+    ):
+        """Call the `/top-headlines` endpoint.
+        Fetch live top and breaking headlines.
+        This endpoint provides live top and breaking headlines for a country, specific category in a country,
+        single source, or multiple sources. You can also search with keywords.  Articles are sorted by the earliest
+        date published first.
+        :param q: Keywords or a phrase to search for in the article title and body.  See the official News API
+            `documentation <https://newsapi.org/docs/endpoints/everything>`_ for search syntax and examples.
+        :type q: str or None
+        :param qintitle: Keywords or a phrase to search for in the article title and body.  See the official News API
+            `documentation <https://newsapi.org/docs/endpoints/everything>`_ for search syntax and examples.
+        :type q: str or None
+        :param sources: A comma-seperated string of identifiers for the news sources or blogs you want headlines from.
+            Use :meth:`NewsApiClient.get_sources` to locate these programmatically, or look at the
+            `sources index <https://newsapi.org/sources>`_.  **Note**: you can't mix this param with the
+            ``country`` or ``category`` params.
+        :type sources: str or None
+        :param language: The 2-letter ISO-639-1 code of the language you want to get headlines for.
+            See :data:`newsapi.const.languages` for the set of allowed values.
+            The default for this method is ``"en"`` (English).  **Note**: this parameter is not mentioned in the
+            `/top-headlines documentation <https://newsapi.org/docs/endpoints/top-headlines>`_ as of Sep. 2019,
+            but *is* supported by the API.
+        :type language: str or None
+        :param country: The 2-letter ISO 3166-1 code of the country you want to get headlines for.
+            See :data:`newsapi.const.countries` for the set of allowed values.
+            **Note**: you can't mix this parameter with the ``sources`` param.
+        :type country: str or None
+        :param category: The category you want to get headlines for.
+            See :data:`newsapi.const.categories` for the set of allowed values.
+            **Note**: you can't mix this parameter with the ``sources`` param.
+        :type category: str or None
+        :param page_size: Use this to page through the results if the total results found is
+            greater than the page size.
+        :type page_size: int or None
+        :param page: The number of results to return per page (request).
+            20 is the default, 100 is the maximum.
+        :type page: int or None
+        :return: JSON response as nested Python dictionary.
+        :rtype: dict
+        :raises NewsAPIException: If the ``"status"`` value of the response is ``"error"`` rather than ``"ok"``.
+        """
+
         payload = {}
 
+        # Keyword/Phrase
         if q is not None:
             if is_valid_string(q):
-                payload['q'] = q
+                payload["q"] = q
             else:
-                TypeError('q must be a valid string')
+                raise TypeError("keyword/phrase q param should be of type str")
 
+        # Keyword/Phrase in Title
         if qintitle is not None:
             if is_valid_string(qintitle):
-                payload['qintitle'] = q
+                payload["qintitle"] = qintitle
             else:
-                TypeError('qintitle must be a valid string')
+                raise TypeError("keyword/phrase qintitle param should be of type str")
 
-        
-        if source is not None and ((country is not None) or language is not None):
-            raise ValueError("The source cannot be passed mixed with coutry or language")
+        # Sources
+        if (sources is not None) and ((country is not None) or (category is not None)):
+            raise ValueError("cannot mix country/category param with sources param.")
 
-        if source is not None:
-            if is_valid_string(source):
-                payload['source'] = source
+        # Sources
+        if sources is not None:
+            if is_valid_string(sources):
+                payload["sources"] = sources
             else:
-                TypeError('q must be a valid string')
-        
+                raise TypeError("sources param should be of type str")
+
+        # Language
         if language is not None:
             if is_valid_string(language):
                 if language in const.languages:
-                    payload['language'] = language
+                    payload["language"] = language
                 else:
-                    ValueError("Language must to be in the sigla format")
+                    raise ValueError("invalid language")
             else:
-                TypeError("Lanauge must to be a valid string")
+                raise TypeError("language param should be of type str")
 
+        # Country
         if country is not None:
             if is_valid_string(country):
                 if country in const.countries:
-                    payload['country'] = country
+                    payload["country"] = country
                 else:
-                    ValueError("Country must to be in the sigla format")
+                    raise ValueError("invalid country")
             else:
-                TypeError("Coutry must to be a valid string")
-        
-        r = self.conxtext_request.get(const.TOP_HEADLINES_URL, self.auth, timeout=30, params=payload)
+                raise TypeError("country param should be of type str")
 
-        if r.request_code != requests.request_code_ok:
-            raise NewsApiExeception(r.json())
+        # Category
+        if category is not None:
+            if is_valid_string(category):
+                if category in const.categories:
+                    payload["category"] = category
+                else:
+                    raise ValueError("invalid category")
+            else:
+                raise TypeError("category param should be of type str")
+
+        # Page Size
+        if page_size is not None:
+            if type(page_size) == int:
+                if 0 <= page_size <= 100:
+                    payload["pageSize"] = page_size
+                else:
+                    raise ValueError("page_size param should be an int between 1 and 100")
+            else:
+                raise TypeError("page_size param should be an int")
+
+        # Page
+        if page is not None:
+            if type(page) == int:
+                if page > 0:
+                    payload["page"] = page
+                else:
+                    raise ValueError("page param should be an int greater than 0")
+            else:
+                raise TypeError("page param should be an int")
+
+        # Send Request
+        r = self.request_method.get(const.TOP_HEADLINES_URL, auth=self.auth, timeout=30, params=payload)
+
+        # Check Status of Request
+        if r.status_code != requests.codes.ok:
+            raise NewsAPIException(r.json())
 
         return r.json()
 
